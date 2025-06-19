be3e054f312c024a36ea6ff6975ccba5d4583787
Xarnak <y.stubio@gmail.com>
2025-06-20 00:17:48 +0500

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,1 +1,21 @@
 # получение погоды с OpenWeatherMap
+import aiohttp
+from config import WEATHER_TOKEN
+
+async def get_weather(city: str):
+    url = f"http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "q": city,
+        "appid": WEATHER_TOKEN,  # Заменить в .env
+        "units": "metric",
+        "lang": "ru"
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params) as resp:
+            data = await resp.json()
+            try:
+                temp = round(data["main"]["temp"])
+                desc = data["weather"][0]["description"]
+                return f"{temp}°C, {desc}"
+            except:
+                return "Погода не найдена"
