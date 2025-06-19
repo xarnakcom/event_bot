dd130eef5816a7deb9888e759be55cbaf8beabc3
Xarnak <y.stubio@gmail.com>
2025-06-20 00:18:24 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 # точка входа, запуск бота
+import asyncio
+from aiogram import Bot, Dispatcher
+from config import BOT_TOKEN
+from db import init_db
+from handlers import admin, foreman, worker
+
+bot = Bot(BOT_TOKEN, parse_mode="HTML")
+dp = Dispatcher()
+
+async def main():
+    await init_db()
+    dp.include_router(admin.router)
+    dp.include_router(foreman.router)
+    dp.include_router(worker.router)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
