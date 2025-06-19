30cada9ea8cccb6b93b621dd8f48ca8a994c9e18
Xarnak <y.stubio@gmail.com>
2025-06-20 00:18:07 +0500

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,1 +1,7 @@
 # клавиатуры с кнопками
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+confirm_kb = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="✅ Да", callback_data="confirm_yes"),
+     InlineKeyboardButton(text="❌ Нет", callback_data="confirm_no")]
+])
