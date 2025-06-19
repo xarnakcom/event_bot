6f26b2fda9971d8396141745319eee77aea1882e
Xarnak <y.stubio@gmail.com>
2025-06-20 00:16:23 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
 # конфиг для загрузки переменных окружения
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # Заменить в .env
+WEATHER_TOKEN = os.getenv("OPENWEATHER_TOKEN")  # Заменить в .env
